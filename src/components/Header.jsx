61ecdfb0231e9898d5f6c019508dadcaca225beb
Simeon Parris <simeonparris@hotmail.com>
2020-05-07 08:41:05 +0100

    Made a small change to Header.jsx to add conditional rendering of a login button or a message depending on the user in App.js state.user

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
 const Header = (props) => {
+    const { user } = props;
     return (
         <header className="heading" >
             <h1>SP NEWS</h1>
             <h2>Where the truth comes out to play...</h2>
-            <button>LOGIN</button>
-            <p>You are logged in as {props.user}</p>
+            {user ? <p>You are logged in as {user}</p> : <button>LOGIN</button>}
         </header>
     );
 };
