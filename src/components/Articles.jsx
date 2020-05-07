b9b28005d4efc4c1100311c5a48d4f6edfde79df
Simeon Parris <simeonparris@hotmail.com>
2020-05-07 11:53:45 +0100

    First commit for deployment repo

--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -17,11 +17,13 @@ class Articles extends Component {
             comment_count: '',
         }],
         isFetching: true,
-        sort_by: 'created_at'
+        sort_by: 'created_at',
+        error: false
     }
 
     render() {
         if (this.state.isFetching === true) return <p>Fetching Articles...</p>
+        if (this.state.error === true) return <ErrorFrame errorMessage={`We don't have any ${this.props.topic_slug} articles.`} />
         return (
             <main >
                 <ArticlesForm updateSortBy={this.updateSortBy} />
@@ -37,22 +39,25 @@ class Articles extends Component {
         const { sort_by } = this.state;
 
         if (topic_slug !== prevProps.topic_slug || sort_by !== prevState.sort_by) {
+
             api.fetchArticles(topic_slug, sort_by).then((response) => {
                 this.setState({ articles: response.articles, isFetching: false })
             }).catch((error) => {
                 return <ErrorFrame error={error} />
             })
+
         }
     }
 
     componentDidMount = () => {
-        console.log('Articles Mounted!')
+
         api.fetchArticles().then(({ articles }) => {
             this.setState({ articles, isFetching: false, currentTopic: 'all' })
 
         }).catch((error) => {
             return <ErrorFrame error={error} />
         })
+
     };
 
     updateSortBy = (sort_by) => {
@@ -60,6 +65,48 @@ class Articles extends Component {
             this.setState({ sort_by })
         };
     };
+
+
 };
 
 export default Articles;
+
+
+////////////////////////
+
+// Attempt at error handling invalid topics
+
+// topicChecker = (topic_slug) => {
+//     const checkedTopic = [];
+//     api.fetchTopics().then((response) => {
+
+//         return response.data.topics.forEach((topic) => {
+//             // console.log(topic.slug, '<-- the current topic slug', topic_slug, '<-- the requested topic slug')
+//             if (topic.slug === topic_slug) {
+//                 checkedTopic.push(topic);
+//             }
+//         })
+//     })
+//     console.log(checkedTopic, '<-- checked topic before return')
+//     return checkedTopic;
+// }
+
+// componentDidUpdate = (prevProps, prevState) => {
+//     const { topic_slug } = this.props;
+//     const { sort_by } = this.state;
+
+//     if (topic_slug !== prevProps.topic_slug || sort_by !== prevState.sort_by) {
+//         console.log(topic_slug, '<-- topic slug')
+//         const checkedTopic = this.topicChecker(topic_slug);
+//         console.log(checkedTopic, '<-- checked topic')
+//         console.log(checkedTopic.length, '<-- checked topic length')
+//         if (checkedTopic.length > 0) {
+//             api.fetchArticles(topic_slug, sort_by).then((response) => {
+//                 this.setState({ articles: response.articles, isFetching: false })
+//             }).catch((error) => {
+//                 return <ErrorFrame error={error} />
+//             })
+//         }
+
+//     }
+// }
